183aa16d056b277e1511ab404cbfc55777331bc2
ml-phan <phanmanhlinh@yahoo.com>
2022-03-31 00:39:17 +0200

    Continue working on process handles and ID

--- bluestacks-old.py
+++ bluestacks-old.py
@@ -6,19 +6,13 @@ import win32api
 import win32con
 import win32gui
 import win32process
-import wmi
 
 
-bs_instance = []
-bs_infos = []
-
-
-def winEnumHandler(hwnd, pname):
-    if win32gui.IsWindowVisible(hwnd) and pname in win32gui.GetWindowText(hwnd):
+def winEnumHandler(hwnd, ctx):
+    if win32gui.IsWindowVisible(hwnd) and win32gui.GetWindowText(hwnd) == "BlueStacks":
         print("Program - ID:", hwnd)
-        print("          WindowText:", win32gui.GetWindowText(hwnd))
-        print("          ClassName:", win32gui.GetClassName(hwnd))
-        bs_instance.append(hwnd)
+        print("        - WindowText:", win32gui.GetWindowText(hwnd))
+        print("        - ClassName:", win32gui.GetClassName(hwnd))
     # with open("windows.txt", "a") as f:
     #     prog_id = "Program - ID: " + str(hex(hwnd))
     #     windowtext = "        - WindowText: " + str(win32gui.GetWindowText(hwnd))
@@ -40,50 +34,14 @@ def click(x, y):
     win32gui.SendMessage(h_wnd1, win32con.WM_LBUTTONDOWN, win32con.MK_LBUTTON, l_param)
     win32gui.SendMessage(h_wnd1, win32con.WM_LBUTTONUP, None, l_param)
 
-
-def get_bs_infos():
-    for hwnd in bs_instance:
-        _, pid = win32process.GetWindowThreadProcessId(hwnd)
-        handle = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION | win32con.PROCESS_VM_READ, 0, pid)
-        handle2 = win32api.OpenProcess(1, False, pid)
-        path = win32process.GetModuleFileNameEx(handle, 0)
-        bs_infos.append([hwnd, pid, handle, handle2, path])
-    return bs_infos
-
-
-def get_processes_command_line(txt):
-    c = wmi.WMI()
-    for process in c.Win32_Process():
-        if isinstance(process.CommandLine, str) and txt in process.CommandLine:
-            print("process.id", process.id)
-            print("process.ProcessID", process.ProcessID)
-            print("Command line", process.CommandLine)
-            # for i in process.properties:
-            #     print(i, getattr(process, i))
-
-
-def find_bs_instances():
-    win32gui.EnumWindows(winEnumHandler, "BlueStacks")
-    get_bs_infos()
-
-
-def kill_bs_2(bit):
-    find_bs_instances()
-    if bit == 32:
-        for i in bs_infos:
-            if "BlueStacks" in i[4] and "bgp64" not in i[4]:
-                terminate_handle = win32api.OpenProcess(win32con.PROCESS_TERMINATE, 0, i[1])
-                win32api.TerminateProcess(terminate_handle, 0)
-                print("Killed BlueStacks 32-bit")
-    elif bit == 64:
-        for i in bs_infos:
-            if "BlueStacks" in i[4] and "bgp64" in i[4]:
-                terminate_handle = win32api.OpenProcess(win32con.PROCESS_TERMINATE, 0, i[1])
-                win32api.TerminateProcess(terminate_handle, 0)
-                print("Killed BlueStacks 64-bit")
-    else:
-        print("No BlueStacks is running")
-    time.sleep(5)
+# while True:
+#     time.sleep(2)
+#     click(200, 200)
+# def restart_bluestacks():
+#     os.system("TASKKILL /F /IM Bluestacks.exe")
+#     time.sleep(5)
+#     os.startfile(r"C:\Program Files\BlueStacks_bgp64\Bluestacks.exe")
+# restart_bluestacks()
 
 
 def get_bs_dimension(hwnd):
@@ -128,24 +86,16 @@ def print_game_info(hwnd):
     print("Game screen height: ", game_height)
 
 
-def start_app(name, bit):
-    if name.lower() == "kok" and bit == 32:
-        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\King of Kinks - 32-bit.lnk")
-    elif name.lower() == "kok" and bit == 64:
-        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\King of Kinks - 64-bit.lnk")
-    elif name.lower() == "qt" and bit == 32:
-        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\Project QT - 32-bit.lnk")
-    elif name.lower() == "qt" and bit == 64:
-        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\Project QT - 64-bit.lnk")
-    elif name.lower() == "ha" and bit == 32:
-        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\Horny Arcana - 32-bit.lnk")
-    elif name.lower() == "ha" and bit == 64:
-        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\Horny Arcana - 64-bit.lnk")
-    else:
-        pass
-    time.sleep(40)
-    find_bs_instances()
-    time.sleep(10)
+def restart_bluestacks():
+    pid = win32gui.FindWindow(None, "BlueStacks")
+    if pid:
+        # win32gui.PostMessage(pid, win32con.WM_CLOSE, 0, 0)
+        t, p = win32process.GetWindowThreadProcessId(pid)
+        handle = win32api.OpenProcess(win32con.PROCESS_TERMINATE, 0, p)
+        win32api.TerminateProcess(handle, 0)
+    time.sleep(5)
+    os.startfile(r"C:\Users\phanm\OneDrive\Desktop\King of Kinks - 64-bit.lnk")
+    time.sleep(50)
 
 
 def to_kok_event(hwnd):
@@ -156,7 +106,7 @@ def to_kok_event(hwnd):
     time.sleep(5)
     print("Click to enter server")
     click(0.5 * game_width, 0.875 * game_height)
-    time.sleep(5)
+    time.sleep(10)
     print("Click to close Mihime sale")
     click(0.9055 * game_width, 0.05125 * game_height)
     time.sleep(2)
@@ -190,62 +140,34 @@ def kok_alchemy_event_tap(hwnd):
 
 def kok_tap(hwnd):
     game_width, game_height = get_game_dimension(hwnd)
+    print("Click to claim Alchemy")
     i = 0
-    print("Starting Tap")
     while i < 600:
         time.sleep(5)
-
         random_width = random.uniform(-0.4, 0.4)
         random_height = random.uniform(-0.14, 0.14)
         click((0.5+random_width) * game_width, (0.155 + random_height) * game_height)
         i += 1
         if i % 100 == 0:
-            print("Tap times:", i)
-
-
-def restart_bluestacks():
-    pid = win32gui.FindWindow(None, "BlueStacks")
-    if pid and pid != "985696":
-        # win32gui.PostMessage(pid, win32con.WM_CLOSE, 0, 0)
-        t, p = win32process.GetWindowThreadProcessId(pid)
-        handle = win32api.OpenProcess(win32con.PROCESS_TERMINATE, 0, p)
-        win32api.TerminateProcess(handle, 0)
-    time.sleep(5)
-    os.startfile(r"C:\Users\phanm\OneDrive\Desktop\King of Kinks - 64-bit.lnk")
-    time.sleep(50)
+            print("Click number:", i)
 
 
 def kok_alchemy_pipeline():
-    kill_bs_2(64)
-    start_app("kok", 64)
-    # restart_bluestacks()
-    pid = 0
-    for i in bs_infos:
-        if "bgp64" in i[4]:
-            pid = i[0]
-    pid2 = win32gui.FindWindow(None, "BlueStacks")
-    time.sleep(1)
-    print("ID from bs_infos:", pid)
-    print("ID from find windows:", pid2)
-    # to_kok_event(pid)
-    # kok_alchemy_event_tap(pid)
-    # kok_tap(pid)
+    restart_bluestacks()
+    pid = win32gui.FindWindow(None, "BlueStacks")
+    to_kok_event(pid)
+    kok_alchemy_event_tap(pid)
+    kok_tap(pid)
     time.sleep(30)
-
 
 
 if __name__ == '__main__':
     # win32gui.EnumWindows(winEnumHandler, None)
-    running = False
     time.sleep(2)
     # restart_bluestacks()
-    bs_instance = []
-    bs_infos = []
-    kok_alchemy_pipeline()
-    times = 1
-    while running:
-        print("Starting run", times)
+    i = 1
+    while True:
+        print("Starting run", i)
         kok_alchemy_pipeline()
-        print("Finished run", times)
-        times += 1
-
+        print("Finished run", i)
+        i += 1
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import os
+import time
+
 import win32api
 import win32con
 import win32gui
@@ -5,26 +8,33 @@ import win32process
 import wmi
 
 bs_instance = []
+bs_infos = []
 
 
 def winEnumHandler(hwnd, pname):
-    if win32gui.IsWindowVisible(hwnd) and pname in win32gui.GetWindowText(hwnd):
+    if win32gui.IsWindowVisible(hwnd) and pname in win32gui.GetWindowText(hwnd)\
+            and "Blue" in win32gui.GetClassName(hwnd):
         print("Program - ID:", hwnd)
         print("          WindowText:", win32gui.GetWindowText(hwnd))
         print("          ClassName:", win32gui.GetClassName(hwnd))
+        print("Class Name Type    :", type(win32gui.GetClassName(hwnd)))
         bs_instance.append(hwnd)
 
 
-def get_bs_instance():
-    pinfo = []
+# Return 5 values
+# 1 - assigned pyhandle
+# 2 - process ID
+# 3 - process handle (to get executable path)
+# 4 - parent process handle (to force close)
+# 5 - process executable path
+def get_bs_infos():
     for hwnd in bs_instance:
         _, pid = win32process.GetWindowThreadProcessId(hwnd)
         handle = win32api.OpenProcess(win32con.PROCESS_QUERY_INFORMATION | win32con.PROCESS_VM_READ, 0, pid)
         handle2 = win32api.OpenProcess(1, False, pid)
         path = win32process.GetModuleFileNameEx(handle, 0)
-        # path2 = win32process.GetModuleFileNameEx(handle2, 0)
-        pinfo.append([hwnd, pid, handle, handle2, path])
-    return pinfo
+        bs_infos.append([hwnd, pid, handle, handle2, path])
+    return bs_infos
 
 
 def get_processes_command_line(txt):
@@ -33,17 +43,71 @@ def get_processes_command_line(txt):
         if isinstance(process.CommandLine, str) and txt in process.CommandLine:
             print("process.id", process.id)
             print("process.ProcessID", process.ProcessID)
-            for i in process.properties:
-                print(i, getattr(process, i))
+            print("Command line", process.CommandLine)
+            # for i in process.properties:
+            #     print(i, getattr(process, i))
 
 
-# hld = win32gui.FindWindow(None, i for "Notepad" in i)
-# print(hld)
-win32gui.EnumWindows(winEnumHandler, "BlueStacks")
-infos = get_bs_instance()
-print(type(infos[0][0]), infos[0][0])
-print(type(infos[0][1]), infos[0][1])
-print(type(infos[0][2]), infos[0][2])
-print(type(infos[0][3]), infos[0][3])
+def find_bs_instances():
+    win32gui.EnumWindows(winEnumHandler, "BlueStacks")
+    get_bs_infos()
+
+
+def kill_bs_2(bit):
+    find_bs_instances()
+    if bit == 32:
+        for i in bs_infos:
+            if "BlueStacks" in i[4] and "bgp64" not in i[4]:
+                terminate_handle = win32api.OpenProcess(win32con.PROCESS_TERMINATE, 0, i[1])
+                win32api.TerminateProcess(terminate_handle, 0)
+                print("Killed BlueStacks 32-bit")
+    elif bit == 64:
+        for i in bs_infos:
+            if "BlueStacks" in i[4] and "bgp64" in i[4]:
+                terminate_handle = win32api.OpenProcess(win32con.PROCESS_TERMINATE, 0, i[1])
+                win32api.TerminateProcess(terminate_handle, 0)
+                print("Killed BlueStacks 64-bit")
+    else:
+        print("No BlueStacks is running")
+
+
+# def kill_bs(bit):
+#
+#     if bit == 32:
+#
+#     elif bit == 64:
+#         for i in bs_infos:
+#             if "bgp64" in i[4]:
+#                 win32process.TerminateProcess(i[3], 0)
+#                 print("Killed BlueStacks 64-bit")
+#     else:
+#         print("No BlueStacks is running")
+
+
+def start_app(name, bit):
+    if name.lower() == "kok" and bit == 32:
+        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\King of Kinks - 32-bit.lnk")
+    elif name.lower() == "kok" and bit == 64:
+        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\King of Kinks - 64-bit.lnk")
+    elif name.lower() == "qt" and bit == 32:
+        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\Project QT - 32-bit.lnk")
+    elif name.lower() == "qt" and bit == 64:
+        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\Project QT - 64-bit.lnk")
+    elif name.lower() == "ha" and bit == 32:
+        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\Horny Arcana - 32-bit.lnk")
+    elif name.lower() == "ha" and bit == 64:
+        os.startfile(r"C:\Users\phanm\OneDrive\Desktop\Horny Arcana - 64-bit.lnk")
+    else:
+        pass
+    time.sleep(40)
+    find_bs_instances()
+    time.sleep(10)
+
+pid = win32gui.FindWindow(None, "BlueStacks")
+print(pid)
+find_bs_instances()
+print(bs_infos)
+
+# os.system("taskkill /f C:\Program Files\BlueStacks_bgp64\Bluestacks.exe")
 # win32process.TerminateProcess(infos[0][2], 0)
-get_processes_command_line("BlueStacks")
+# get_processes_command_line("HD-Player")
